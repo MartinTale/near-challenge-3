cb46fd7a3dc5c3f3fd3a89a4121bcdfd682cc93c
Martin Tale <me@martintale.com>
2021-11-19 22:57:42 +0000

    another fix

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import 'regenerator-runtime/runtime'
 import { initContract, login, logout } from './utils'
 
 import getConfig from './config'
-const { networkId } = getConfig(process.env.NODE_ENV || 'development')
+const { networkId } = getConfig('development')
 
 // global variable used throughout
 let currentGreeting
